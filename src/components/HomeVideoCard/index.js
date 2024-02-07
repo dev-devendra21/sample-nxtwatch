629f7b0d95abb62106d951b5661224c6f5d3f430
Devendra <devendrakumaraa@gmail.com>
2024-02-07 12:15:47 +0000

    solution

---
+++ src/components/HomeVideoCard/index.js
@@ -0,0 +1,55 @@
+import ThemeAndVideoContext from '../../context/ThemeAndVideoContext'
+
+import {
+  ListItem,
+  ThumbNailImage,
+  VideoDetails,
+  ProfileImage,
+  ContentSection,
+  Title,
+  ChannelName,
+  ViewsAndDate,
+  Dot,
+  LinkElement,
+} from './styledComponents'
+
+const HomeVideoCard = props => {
+  const {video} = props
+  const {
+    id,
+    title,
+    thumbnailUrl,
+    viewCount,
+    publishedAt,
+    name,
+    profileImageUrl,
+  } = video
+  return (
+    <ThemeAndVideoContext.Consumer>
+      {value => {
+        const {isDarkTheme} = value
+        const textColor = isDarkTheme ? '#f9f9f9' : '#231f20'
+
+        return (
+          <LinkElement to={`/videos/${id}`} className="link">
+            <ListItem>
+              <ThumbNailImage src={thumbnailUrl} alt="video thumbnail" />
+              <VideoDetails>
+                <ProfileImage src={profileImageUrl} alt="channel logo" />
+                <ContentSection>
+                  <Title color={textColor}>{title}</Title>
+                  <ChannelName color={textColor}>{name}</ChannelName>
+                  <ViewsAndDate color={textColor}>
+                    {viewCount} views<Dot> &#8226; </Dot> {publishedAt}
+                  </ViewsAndDate>
+                </ContentSection>
+              </VideoDetails>
+            </ListItem>
+          </LinkElement>
+        )
+      }}
+    </ThemeAndVideoContext.Consumer>
+  )
+}
+
+export default HomeVideoCard
---
+++ src/components/SavedVideos/index.js
@@ -0,0 +1,66 @@
+import {CgPlayListAdd} from 'react-icons/cg'
+import Header from '../Header'
+import NavigationBar from '../NavigationBar'
+import ThemeAndVideoContext from '../../context/ThemeAndVideoContext'
+import VideoCard from '../VideoCard'
+import {
+  SavedContainer,
+  SavedTitleIconContainer,
+  SavedVideoTitle,
+  SavedVideoList,
+  SavedText,
+  NoSavedVideosView,
+  NoSavedVideosImage,
+  NoSavedVideosHeading,
+  NoSavedVideosNote,
+} from './styledComponents'
+
+const SavedVideos = () => (
+  <ThemeAndVideoContext.Consumer>
+    {value => {
+      const {isDarkTheme, savedVideos} = value
+      const bgColor = isDarkTheme ? '#1e293b' : '#f5f5f5'
+      const textColor = isDarkTheme ? '#ffffff' : '#000000'
+      const headingColor = isDarkTheme ? '#ffffff' : '#000000'
+      const noteColor = isDarkTheme ? '#475569' : '#808080'
+
+      return (
+        <>
+          <Header />
+          <NavigationBar />
+          <SavedContainer data-testid="savedvideos" bgColor={bgColor}>
+            <SavedVideoTitle>
+              <SavedTitleIconContainer>
+                <CgPlayListAdd size={35} color="#ff0000" />
+              </SavedTitleIconContainer>
+              <SavedText color={textColor}>Saved Videos</SavedText>
+            </SavedVideoTitle>
+
+            {savedVideos.length > 0 ? (
+              <SavedVideoList>
+                {savedVideos.map(eachVideo => (
+                  <VideoCard key={eachVideo.id} videoDetails={eachVideo} />
+                ))}
+              </SavedVideoList>
+            ) : (
+              <NoSavedVideosView>
+                <NoSavedVideosImage
+                  src="https://assets.ccbp.in/frontend/react-js/nxt-watch-no-saved-videos-img.png"
+                  alt="no saved videos"
+                />
+                <NoSavedVideosHeading headingColor={headingColor}>
+                  No saved videos found
+                </NoSavedVideosHeading>
+                <NoSavedVideosNote noteColor={noteColor}>
+                  You can save your videos while watching them
+                </NoSavedVideosNote>
+              </NoSavedVideosView>
+            )}
+          </SavedContainer>
+        </>
+      )
+    }}
+  </ThemeAndVideoContext.Consumer>
+)
+
+export default SavedVideos
